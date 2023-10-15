ff1e935587422002ae278f31297af854a6cf67f0
kashima19960 <2494389920@qq.com>
2023-10-15 15:32:58 +0800

    完成稀疏矩阵的三元法压缩

--- include/Matrix.h
+++ include/Matrix.h
@@ -3,8 +3,30 @@
 #include <cstring>
 #include <vector>
 #define Demension 3
+#define MAXNUMBER 50
 using std::cerr;
 using std::cin;
 using std::cout;
 using std::endl;
+class triple
+{
+public:
+    // 矩阵行标
+    int i;
+    // 矩阵列标
+    int j;
+    // 数据
+    int data;
+};
+class TSMatrix
+{
+public:
+    triple data[MAXNUMBER];
+    int Row;  // 矩阵的行数
+    int Cols; // 矩阵的列数
+    int size; // 非零元素的个数
+    void Store(int row, int col, int data);
+    TSMatrix(int row, int cols);
+    void display();
+};
 void MatrixTest(void);
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -29,11 +29,65 @@ bool SymmetricMatrixCondense(T (*matrix)[Demension])
     }
     return true;
 }
+// 稀疏矩阵的三元组压缩法
+TSMatrix::TSMatrix(int row, int cols)
+{
+    size = 0;
+    this->Cols = cols;
+    this->Row = row;
+}
+void TSMatrix::display()
+{
+    for (int i = 0; i < this->Row; i++)
+    {
+        for (int j = 0; j < this->Cols; j++)
+        {
+            int flag = 0;
+            for (int k = 0; k < this->size; k++)
+            {
+                if (i == this->data[k].i && j == this->data[k].j)
+                {
+                    cout << this->data[k].data << " ";
+                    flag = 1;
+                }
+            }
+            if (flag == 0)
+            {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+}
+void TSMatrix::Store(int row, int col, int data)
+{
+    if (row > this->Row || col > this->Cols || data == 0)
+    {
+        cerr << "parameter make no sense!" << endl;
+        exit(0);
+    }
+    this->data[size].i = row;
+    this->data[size].j = col;
+    this->data[size].data = data;
+    size++;
+}
+
+// 测试函数
 void MatrixTest(void)
 {
-    int test[3][3] = {
-        {1, 2, 3},
-        {2, 1, 4},
-        {3, 4, 1}};
-    SymmetricMatrixCondense<int>(test);
+    int a[3][4] = {
+        {1, 2, 3, 4},
+        {0, 0, 0, 0},
+        {0, 1, 0, 3},
+    };
+    TSMatrix test(3, 4);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (a[i][j] != 0)
+                test.Store(i, j, a[i][j]);
+        }
+    }
+    test.display();
 }
