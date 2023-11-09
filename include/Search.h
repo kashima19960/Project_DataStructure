14b2ac1b2704c336670bee74360a358c01b6b171
kashima19960 <2494389920@qq.com>
2023-11-09 20:25:39 +0800

    二分查找

--- include/Search.h
+++ include/Search.h
@@ -7,4 +7,5 @@ using std::cout;
 using std::endl;
 template <typename T>
 int Sec_Search(T arr[], int n, T value);
+int Binary_Search(int arr[], int n, int value);
 void Search_Test(void);
--- src/Search.cpp
+++ src/Search.cpp
@@ -26,11 +26,35 @@ ListNode<T> *Link_Search(LinkList<T> *list, T value)
     }
     return NULL;
 }
+
+/*
+二分查找限制只能使用顺序存储结构,并且假定查找的序列是有序的
+*/
+
+int Binary_Search(int arr[], int n, int value)
+{
+    int low = 0, high = n;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == value)
+        {
+            return mid;
+        }
+        else if (arr[mid] > value)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
 void Search_Test(void)
 {
-    LinkList<int> test;
-    for (int i = 0; i < 10; i++)
-        test.Insert(0, i);
-
-    cout << Link_Search<int>(&test, 11)->data;
+    int test[4] = {1, 3, 5, 7};
+    cout << Binary_Search(test, 4, 5);
 }
